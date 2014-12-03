6589fecea0ec5f2bdca96204d8b4746269240907
Taichi-Systems <Cubid_10411@me.com>
2014-12-03 19:22:21 +0900

    床とかプレイヤー移動とか

---
+++ Assets/Scripts/MapGen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGen : MonoBehaviour {
+	public static int MAP_LENGTH_WIDTH = 20;
+	public static int MAP_LENGTH_HEIGHT = 10;
+	public static int CELL_SIZE = 100;
+
+	public GameObject floorPrefab;
+
+	int xpos = 0;
+	int ypos = 0;
+
+	void Start () {
+		floorPrefab = (GameObject)Resources.Load("MapObjects/floor");
+		for (int i=0; i<MAP_LENGTH_HEIGHT; i++) {
+			ypos+=CELL_SIZE;
+			xpos=0;
+			for(int j=0;j<MAP_LENGTH_WIDTH;j++){
+				xpos+=CELL_SIZE;
+				Instantiate(this.floorPrefab,new Vector3(xpos-(CELL_SIZE/2),ypos-(CELL_SIZE/2),-1),Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+	public static float SPEED = 1500;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = new Vector2 (Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")).normalized;
+		rigidbody2D.velocity = direction * SPEED;
+	}
+}

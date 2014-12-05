082cb98a02551c7bc017ccf6579ce3306c8d3384
Taichi-Systems <Cubid_10411@me.com>
2014-12-05 18:27:49 +0900

    プレイヤーの指定移動，ジャンプ．

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -5,11 +5,12 @@ public class CameraBehaviour : MonoBehaviour {
 	public GameObject player;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3 (player.transform.position.x,player.transform.position.y,-10);
 	}
+
 }
--- Assets/Scripts/MapGen.cs
+++ Assets/Scripts/MapGen.cs
@@ -5,26 +5,38 @@ public class MapGen : MonoBehaviour {
 	public static int MAP_LENGTH_WIDTH = 20;
 	public static int MAP_LENGTH_HEIGHT = 10;
 	public static int CELL_SIZE = 100;
+	public static Vector2[,] PositionMatrix= new Vector2[MAP_LENGTH_HEIGHT, MAP_LENGTH_WIDTH];
+
 
 	public GameObject floorPrefab;
+	public GameObject playerPrefab;
+	public GameObject main_cameraPrefab;
 
 	int xpos = 0;
 	int ypos = 0;
 
 	void Start () {
 		floorPrefab = (GameObject)Resources.Load("MapObjects/floor");
+		playerPrefab = (GameObject)Resources.Load ("Charactors/Player");
+
 		for (int i=0; i<MAP_LENGTH_HEIGHT; i++) {
 			ypos+=CELL_SIZE;
 			xpos=0;
 			for(int j=0;j<MAP_LENGTH_WIDTH;j++){
 				xpos+=CELL_SIZE;
 				Instantiate(this.floorPrefab,new Vector3(xpos-(CELL_SIZE/2),ypos-(CELL_SIZE/2),-1),Quaternion.identity);
+				PositionMatrix[i,j] = new Vector2(xpos-(CELL_SIZE/2),ypos-(CELL_SIZE/2));
 			}
 		}
+
+		Instantiate (this.playerPrefab,PositionMatrix[5,10],Quaternion.identity);
+		main_cameraPrefab = (GameObject)Resources.Load ("Camera/Main_Camera");
+		Instantiate (this.main_cameraPrefab,PositionMatrix[5,10],Quaternion.identity);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }

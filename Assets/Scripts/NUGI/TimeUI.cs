ee28e973919af4079548cf89f8f34f7bb9878d2e
SP2LC <sp2lc.sp@gmail.com>
2015-02-03 16:21:09 +0900

    秒数バーの表示(試験的)

---
+++ Assets/Scripts/NUGI/TimeUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeUI : MonoBehaviour {
+
+	public float time_limet = 10;
+	float time = 0,start_time;
+
+	UISprite sprite;
+	// Use this for initialization
+	void Start () {
+		start_time = Time.time;
+		sprite = this.GetComponent<UISprite>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float temp = Time.time - start_time;
+		sprite.fillAmount = 1 -  temp / time_limet;
+		if(temp > time_limet){
+			 start_time = Time.time;
+		}	
+	}
+}

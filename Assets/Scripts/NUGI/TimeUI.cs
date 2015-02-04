6fcbaffbb2f32551429ae2d73180cebc6b41d169
SP2LC <sp2lc.sp@gmail.com>
2015-02-04 16:31:21 +0900

    time counter と プレイヤーの挙動の関連

--- Assets/Scripts/NUGI/TimeUI.cs
+++ Assets/Scripts/NUGI/TimeUI.cs
@@ -3,22 +3,31 @@ using System.Collections;
 
 public class TimeUI : MonoBehaviour {
 
-	public float time_limet = 10;
-	float time = 0,start_time;
+	public static float time_limet = 10;
+	public static float time = 0,start_time;
+
+	int now_turn;
+	int old_turn;
 
 	UISprite sprite;
 	// Use this for initialization
 	void Start () {
 		start_time = Time.time;
 		sprite = this.GetComponent<UISprite>();
+		now_turn = old_turn= turn.turn_count;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		now_turn = turn.turn_count;
+
 		float temp = Time.time - start_time;
 		sprite.fillAmount = 1 -  temp / time_limet;
-		if(temp > time_limet){
-			 start_time = Time.time;
-		}	
-	}
+		if(temp > time_limet)	 start_time = Time.time;
+		
+		if(now_turn != old_turn) start_time = Time.time;
+
+		old_turn = now_turn;
+
+	}	
 }
